7be78f59bbd94049d5cb6b3ae0b52d71ccf03314
CZnyu <CZnyu@users.noreply.github.com>
2020-06-06 12:27:58 -0400

    Changes Week 2 of Class

---
+++ app/python_practice.py
@@ -0,0 +1,35 @@
+
+
+
+
+x = 1
+print(x)
+
+
+breakpoint()
+
+
+x = 10
+print(x)
+
+y = 5
+
+print(y)
+
+
+y = y + 100
+print(y)
+
+y += 100
+print(y)
+
+print(1, 2, 3)
+
+z = 12
+
+if z == 37:
+    print("YES")
+else:
+    print("NO")
+
+
